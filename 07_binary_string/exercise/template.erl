89bc87aad213c725cf81162f7c22157532125d97
Andrei Dziaineka <dziaineka@gmail.com>
2019-04-07 00:10:44 +0300

    uncompleted solution

--- 07_binary_string/exercise/template.erl
+++ 07_binary_string/exercise/template.erl
@@ -3,4 +3,31 @@
 -export([parse/2]).
 
 parse(Str, Data) when is_binary(Str) ->
-    Str.
+    maps:fold(
+        fun (Key, Value, BinString) ->
+            Placeholder = [<<"{{">>, Key, <<"}}">>],
+
+            if
+                is_binary(Value) ->
+                    Substitute = Value;
+
+                is_list(Value) ->
+                    Substitute = unicode:characters_to_binary(Value);
+
+                is_integer(Value) ->
+                    Substitute = unicode:characters_to_binary(
+                                                    integer_to_list(Value));
+
+                true ->
+                    Substitute = <<"">>
+            end,
+
+            binary:replace(BinString,
+                           iolist_to_binary(Placeholder),
+                           Substitute,
+                           [global])
+        end,
+        Str,
+        Data
+    ).
+
