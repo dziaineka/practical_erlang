2b1d0add7f15013aa0ee00f83b1b755c5c7037c2
Andrei Dziaineka <dziaineka@gmail.com>
2019-04-07 10:49:37 +0300

    exercise completed

--- 07_binary_string/exercise/template.erl
+++ 07_binary_string/exercise/template.erl
@@ -2,32 +2,36 @@
 
 -export([parse/2]).
 
+
+get_binary_value(Key, Map) ->
+    Value = maps:get(Key, Map, <<"">>),
+
+    if
+        is_binary(Value) ->
+            Value;
+
+        is_list(Value) ->
+            unicode:characters_to_binary(Value);
+
+        is_integer(Value) ->
+            unicode:characters_to_binary(integer_to_list(Value));
+
+        true ->
+            <<"">>
+    end.
+
 parse(Str, Data) when is_binary(Str) ->
-    maps:fold(
-        fun (Key, Value, BinString) ->
-            Placeholder = [<<"{{">>, Key, <<"}}">>],
+    case binary:match(Str, <<"{{">>) of
+        nomatch ->
+            Str;
 
-            if
-                is_binary(Value) ->
-                    Substitute = Value;
-
-                is_list(Value) ->
-                    Substitute = unicode:characters_to_binary(Value);
-
-                is_integer(Value) ->
-                    Substitute = unicode:characters_to_binary(
-                                                    integer_to_list(Value));
-
-                true ->
-                    Substitute = <<"">>
-            end,
-
-            binary:replace(BinString,
-                           iolist_to_binary(Placeholder),
-                           Substitute,
-                           [global])
-        end,
-        Str,
-        Data
-    ).
+        _ ->
+            [Begin, UnsplittedRest] = binary:split(Str, [<<"{{">>]),
+            [Placeholder, End] = binary:split(UnsplittedRest, [<<"}}">>]),
+            parse(
+                iolist_to_binary(
+                    [Begin, get_binary_value(Placeholder, Data), End]
+                ),
+                Data)
+    end.
 
