020cb0b39ace57febe488208c85bc04ad33d43e0
Andrei Dziaineka <dziaineka@gmail.com>
2019-04-14 14:23:50 +0300

    chat user module implemented

--- 10_gen_server_2/exercise/chat_user.erl
+++ 10_gen_server_2/exercise/chat_user.erl
@@ -3,3 +3,52 @@
 
 -export([start_link/0, add_message/3, get_messages/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-record(state, {messages = []}).
+
+
+%%%%%%%% client side %%%%%%%%
+
+start_link() ->
+    gen_server:start_link(?MODULE, [], []).
+
+
+add_message(UserPid, Author, Message) ->
+    gen_server:call(UserPid, {add_message, UserPid, Author, Message}).
+
+get_messages(UserPid) ->
+    gen_server:call(UserPid, {get_messages, UserPid}).
+
+
+%%%%%%%% server side %%%%%%%%
+
+init([]) ->
+    {ok, #state{}}.
+
+
+handle_call({add_message, _UserPid, Author, Message}, _From, State) ->
+    NewState = State#state{
+        messages=[{Author, Message} | State#state.messages]
+    },
+    {reply, ok, NewState};
+
+handle_call({get_messages, _UserPid}, _From, State) ->
+    {reply, State#state.messages, State}.
+
+
+handle_cast(_Msg, State) ->
+   {noreply, State}.
+
+handle_info(_Info, State) ->
+   {noreply, State}.
+
+terminate(_Reason, _State) ->
+   ok.
+
+code_change(_OldVsn, State, _Extra) ->
+   {ok, State}.
+
+
+
+
+
