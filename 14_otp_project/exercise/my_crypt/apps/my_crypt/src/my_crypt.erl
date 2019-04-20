17c1b7c31bf59b1f48553c1bed8616ce5fa1c4d0
Andrei Dziaineka <dziaineka@gmail.com>
2019-04-20 22:45:10 +0300

    crypto abilities added

--- 14_otp_project/exercise/my_crypt/apps/my_crypt/src/my_crypt.erl
+++ 14_otp_project/exercise/my_crypt/apps/my_crypt/src/my_crypt.erl
@@ -11,7 +11,7 @@
 -record(state, {key, hash_size}).
 
 start_link() ->
-    gen_server:start_link(?MODULE, [], []).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 
 encode(Data) ->
@@ -36,15 +36,64 @@ init(_Args) ->
     {ok, Key} = application:get_env(crypt_key),
     {ok, HashSize} = application:get_env(hash_size),
     io:format("~p~n~p~n", [Key, HashSize]),
-    {ok, #state{key=Key, hash_size=HashSize}}.
+    {ok, #state{key=binary_to_list(Key), hash_size=HashSize}}.
+
+
+get_crypt_key_symbol(Key, Index) ->
+    KeyLength = length(Key),
+
+    if
+        Index =< KeyLength ->
+            lists:nth(Index, Key);
+
+        true ->
+            lists:nth(Index rem KeyLength, Key)
+    end.
+
+
+xor_list([], _Key, _Index) ->
+    [];
+
+xor_list([Head | Tail], Key, Index) ->
+    [
+        (Head bxor get_crypt_key_symbol(Key, Index)) |
+        xor_list(Tail, Key, Index + 1)
+    ].
+
+
+encrypt(Key, Data) ->
+    DataList = binary_to_list(Data),
+    StartIndex = 1,
+    EncryptedData = xor_list(DataList, Key, StartIndex),
+    list_to_binary(EncryptedData).
+
+
+get_hash(HashSize, Data) ->
+    lists:foldl(
+        fun
+            (Char, Hash) ->
+                ((Hash * 17) + Char) div HashSize
+        end,
+        0,
+        Data
+    ).
 
 
 handle_call({get_key}, _From, State) ->
     {reply, State#state.key, State};
 
 handle_call({set_key, Key}, _From, State) ->
-    NewState = State#state{key=Key},
-    {reply, ok, NewState}.
+    NewState = State#state{key=binary_to_list(Key)},
+    {reply, ok, NewState};
+
+handle_call({encode, InitialData}, _From, State) ->
+    EncryptedData = encrypt(State#state.key, InitialData),
+    {reply, EncryptedData, State};
+
+handle_call({hash, InitialData}, _From, State) ->
+    DataList = binary_to_list(InitialData),
+    Hash = get_hash(State#state.hash_size, DataList),
+    {reply, Hash, State}.
 
 
 handle_cast(_Msg, State) ->
